fca8bfc859d0fb98787c54f09531ba88cb703e18
RamySy <45522076+RamySy@users.noreply.github.com>
2019-12-10 02:05:07 +0100

    SavePoint Before Activity Error

---
+++ app/src/main/java/com/resto/project/Viewholder/FoodViewHolder.java
@@ -0,0 +1,38 @@
+package com.resto.project.Viewholder;
+
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.resto.project.Interface.ItemClickListener;
+
+import com.resto.project.R;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public class FoodViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+    public TextView food_name;
+    public ImageView food_image;
+
+    private ItemClickListener itemClickListener;
+
+    public void setItemClickListener(ItemClickListener itemClickListener) {
+        this.itemClickListener = itemClickListener;
+
+
+    }
+
+    public FoodViewHolder(View itemView) {
+        super(itemView);
+
+        food_name = (TextView)itemView.findViewById(R.id.food_name);
+        food_image = (ImageView)itemView.findViewById(R.id.food_image);
+
+        itemView.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        itemClickListener.onClick(v,getAdapterPosition(),false);
+    }
+}
